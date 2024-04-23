47ea9eb956a3ac93f928de8179a3bada81c1e6ec
vikashrajverma89 <135321501+vikashrajverma89@users.noreply.github.com>
2024-04-24 04:50:42 +0530

    Add files via upload

---
+++ my sql slect.sql
@@ -0,0 +1,19 @@
+select * 
+FROM parks_and_recreation.employee_demographics
+
+
+select first_name,
+last_name,
+birth_date,
+age,
+(age + 10) * 10 + 10
+FROM parks_and_recreation.employee_demographics;
+
+
+SELECT DISTINCT first_name, gender
+FROM parks_and_recreation.employee_demographics;
+
+
+
+
+
